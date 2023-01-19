e44bf5002791e0c2c6dcbb6cf45f978a1f841009
Tsyriulnikov <max_djvu@mail.ru>
2023-01-19 15:15:56 +0300

    dest ver3

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -26,7 +26,10 @@ export class MapComponent implements AfterViewInit {
   private map: any
 
   private initMap(): void {
-   
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    })
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
